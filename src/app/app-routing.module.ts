1d041e8f3c48f7ba92f2c5e0fc63d251d3248904
Dave <dave@webdave.de>
2019-04-01 20:46:51 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FormularComponent } from './formular/formular.component';
 import { ProgressComponent } from './progress/progress.component';
-import { MylistComponent } from './mylist/mylist.component';
 
 const routes: Routes = [
   {
